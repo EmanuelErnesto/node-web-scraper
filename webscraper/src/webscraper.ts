8b1d94e58bf8486cbd88b2a378841833ae7be7c4
EmanuelErnesto <emanuel.earego@gmail.com>
2025-03-19 15:52:24 -0300

    feat(webscraper): add wrapper to puppeteer methods

---
+++ webscraper/src/webscraper.ts
@@ -0,0 +1,23 @@
+import puppeteer, { Browser, Page } from 'puppeteer';
+
+export class WebScraper {
+  public static async initBrowser(): Promise<Browser> {
+    return await puppeteer.launch({
+      headless: false,
+      args: ['--start-maximized']
+    });
+  }
+
+  public static async createPage(browser: Browser): Promise<Page> {
+    return await browser.newPage();
+  }
+
+  public static async accessUrl(page: Page, url: string): Promise<void> {
+    await page.setViewport({width: 1920, height: 1080});
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+  }
+
+  public static async closeBrowser(browser: Browser): Promise<void> {
+    await browser.close();
+  }
+}
